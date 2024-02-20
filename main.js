91bb7f338dbe2b3c17fee9b86ace6740bee69bf9
harmonydigital <131012395+harmonydigital@users.noreply.github.com>
2024-02-20 12:07:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -24,12 +24,12 @@
      var itelefone=document.getElementById('itelefone').value
      var icpf=document.getElementById('icpf').value
 
-     step1=[{
+     step1={
        name:iname,
        email:iemail,
        telefone:itelefone,
        cpf:icpf  
-     }]
+     }
      
       localStorage.setItem(step, Array.from(step1)); 
 
@@ -67,13 +67,60 @@
      },1000);
    }
    if(step=="step-3"){  
+     
+            
+   // ===============================step2==============================
+     var irg=document.getElementById('irg').value
+     var iorgaoemissor=document.getElementById('iorgaoemissor').value
+     var idataemissao=document.getElementById('idataemissao').value
+     var inomemae=document.getElementById('inomemae').value
+     var inomepai=document.getElementById('inomepai').value
+
+     step3=[{
+       rg:irg,
+       orgaoemissor:iorgaoemissor,
+       dataemissao:idataemissao,
+       nomemae:inomemae,
+       nomepai:inomepai
+     }]
+     
+      localStorage.setItem(step, Array.from(step3)); 
+
+     // ===============================step1==============================
      setTimeout(()=>{
       window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/step-4.html" 
      },1000);
    }
    if(step=="step-4"){  
+     
+     console.log(localStorage.getItem(step1)); 
+     
+      // ===============================step2==============================
+     var irefbancarias=document.getElementById('irefbancarias').value
+     var iemprestimos=document.getElementById('iemprestimos').value
+     var iempinstituicao=document.getElementById('iempinstituicao').value
+     var ivaloremp=document.getElementById('ivaloremp').value
+     var ivaloremp=document.getElementById('iparcelas').value
+     var idataemp=document.getElementById('idataemp').value
+     var iqtdparcelas=document.getElementById('iqtdparcelas').value
+
+     step4=[{
+       refbancarias:irefbancarias,
+       emprestimos:iemprestimos,
+       empinstituicao:iempinstituicao,
+       ivaloremp:ivaloremp,
+       ivaloremp:ivaloremp,
+       idataemp:idataemp,
+       iqtdparcelas:iqtdparcelas       
+  
+     }]
+     
+      localStorage.setItem(step, Array.from(step4)); 
+      console.log('step4',step4)
+     // ===============================step1==============================
+     
      setTimeout(()=>{
-        window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/sucess.html" 
+        // window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/sucess.html" 
      },1000);
    }
 
