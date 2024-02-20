784139575159e503aa7cc55b3b9c2862c5320be7
harmonydigital <131012395+harmonydigital@users.noreply.github.com>
2024-02-20 11:13:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,63 +1,83 @@
-const form = document.getElementById ("dataSubmit")
+  const form = document.getElementById ("dataSubmit")
 
-let inputs=document.querySelectorAll("input")
-let msg=document.getElementById ("msg")
-let arrInputs=Array.from(inputs)
-let step=document.querySelector('form').getAttribute('step')
-let jsonArr=[]
-var step1=[] 
+  let inputs=document.querySelectorAll("input")
+  let msg=document.getElementById ("msg")
+  let arrInputs=Array.from(inputs)
+  let step=document.querySelector('form').getAttribute('step')
+  let jsonArr=[]
+  var step1=[]
+  var step2=[]
+  var step3=[]
+      
+  var step4=[] 
 
-document.querySelector('form').addEventListener('submit', (e)=>{
-  e.preventDefault()
-    
-  
-  
- if(step=="step-1"){  
-    
-   var iname=document.getElementById('iname').value
-   var iemail=document.getElementById('iemail').value
-   var itelefone=document.getElementById('itelefone').value
-   var icpf=document.getElementById('icpf').value
-   
-   step1=[{
-     name:iname,
-     email:iemail,
-     telefone:itelefone,
-     cpf:icpf  
-   }]
-   
-   // ===============================step1==============================
-   
-   setTimeout(()=>{
-      // window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/step-2.html" 
+  document.querySelector('form').addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+
+
+   if(step=="step-1"){ 
      
-     console.log(step1)
-    
-   },1000);
- }
- if(step=="step-2"){  
-   setTimeout(()=>{
-    window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/step-3.html" 
-   },1000);
- }
- if(step=="step-3"){  
-   setTimeout(()=>{
-    window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/step-4.html" 
-   },1000);
- }
- if(step=="step-4"){  
-   setTimeout(()=>{
-      window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/sucess.html" 
-   },1000);
- }
-  
-  
+   // ===============================step1==============================
+     var iname=document.getElementById('iname').value
+     var iemail=document.getElementById('iemail').value
+     var itelefone=document.getElementById('itelefone').value
+     var icpf=document.getElementById('icpf').value
+
+     step1=[{
+       name:iname,
+       email:iemail,
+       telefone:itelefone,
+       cpf:icpf  
+     }]
+     
+      localStorage.setItem(step1, step1); 
+
+     // ===============================step1==============================
+
+     setTimeout(()=>{
+         window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/step-2.html" 
+
+      
+     },1000);
+   }
+   if(step=="step-2"){  
+     
+          
+   // ===============================step2==============================
+     var irg=document.getElementById('irg').value
+     var iorgaoemissor=document.getElementById('iorgaoemissor').value
+     var idataemissao=document.getElementById('idataemissao').value
+     var inomemae=document.getElementById('inomemae').value
+     var inomepai=document.getElementById('inomepai').value
+
+     step2=[{
+       rg:irg,
+       orgaoemissor:iorgaoemissor,
+       dataemissao:idataemissao,
+       nomemae:inomemae,
+       nomepai:inomepai
+     }]
+     
+      localStorage.setItem(step2, step2); 
+
+     // ===============================step1==============================
+     setTimeout(()=>{
+      window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/step-3.html" 
+     },1000);
+   }
+   if(step=="step-3"){  
+     setTimeout(()=>{
+      window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/step-4.html" 
+     },1000);
+   }
+   if(step=="step-4"){  
+     setTimeout(()=>{
+        window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/sucess.html" 
+     },1000);
+   }
+
+
+
  
- jsonArr.forEach((element, index, array) => {
-    // console.log(element); // 100, 200, 300
-    // console.log(index); // 0, 1, 2
-    // console.log(array); // same myArray object 3 times
- 
-    console.log("this",element)
-});
-})
+  })
