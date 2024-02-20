5b7f6c7c44ada6226c18e2845c59b12eddcc1871
harmonydigital <131012395+harmonydigital@users.noreply.github.com>
2024-02-20 10:48:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,24 +5,34 @@ let msg=document.getElementById ("msg")
 let arrInputs=Array.from(inputs)
 let step=document.querySelector('form').getAttribute('step')
 let jsonArr=[]
- 
+var step1=[] 
 
 document.querySelector('form').addEventListener('submit', (e)=>{
   e.preventDefault()
     
-  arrInputs.map((mapInp)=>{ 
-        var name=mapInp.getAttribute('name')
-        jsonArr.push({
-        [mapInp.getAttribute('name')]:mapInp.value, 
-    }); 
-  }) 
   
-  localStorage.setItem(step, Array.from(jsonArr)); 
   
  if(step=="step-1"){  
+    
+   var iname=document.getElementById('iname').value
+   var iemail=document.getElementById('iemail').value
+   var itelefone=document.getElementById('itelefone').value
+   var icpf=document.getElementById('icpf').value
+   
+   step1=[{
+     name:iname,
+     email:iemail,
+     telefone:itelefone,
+     cpf:icpf  
+   }]
+   
+   // ===============================step1==============================
+   
    setTimeout(()=>{
-    // window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/step-2.html" 
-     // console.log(localStorage.getItem(step))
+      // window.location.href="https://harmonydigital.github.io/Cadastro-Prianti/step-2.html" 
+     
+     console.log(step1)
+    
    },1000);
  }
  if(step=="step-2"){  
@@ -42,12 +52,12 @@ document.querySelector('form').addEventListener('submit', (e)=>{
  }
   
   
- console.log(jsonArr);
- jsonArr.forEach((element, index, array) => {
-     console.log(element); // 100, 200, 300
-      console.log(index); // 0, 1, 2
-     console.log(array); // same myArray object 3 times
  
-     
+ jsonArr.forEach((element, index, array) => {
+    // console.log(element); // 100, 200, 300
+    // console.log(index); // 0, 1, 2
+    // console.log(array); // same myArray object 3 times
+ 
+    console.log("this",element)
 });
 })
